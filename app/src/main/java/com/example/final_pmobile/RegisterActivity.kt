796492d5213c16e91324884ad632fb1d185504ae
aswarthedoctor@gmail.com <68291451+Aswar12@users.noreply.github.com>
2021-07-26 10:51:56 +0800

    barui halaman regis

--- app/src/main/java/com/example/final_pmobile/RegisterActivity.kt
+++ app/src/main/java/com/example/final_pmobile/RegisterActivity.kt
@@ -9,42 +9,55 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import java.util.regex.Pattern
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var etEmail:EditText
+    private lateinit var etPassword: EditText
+    private lateinit var btnSingUp: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
          auth = FirebaseAuth.getInstance()
-        var btn_singup: Button
-        btn_singup = findViewById(R.id.btn_singup)
-         btn_singup.setOnClickListener {
+        etEmail= findViewById(R.id.editTextEmail)
+        etPassword= findViewById(R.id.editTextpassword)
+        btnSingUp = findViewById(R.id.btn_singup)
+        btnSingUp.setOnClickListener {
+            val email= etEmail.text.toString().trim()
+            val password= etPassword.text.toString().trim()
+            if (email.isEmpty() ){
+                etEmail.error="Masukkan Email"
+                etEmail.requestFocus()
+                return@setOnClickListener
+            }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    etEmail.error="Email tidak valid"
+                    etEmail.requestFocus()
+                    return@setOnClickListener
+                }
 
 
-             val  email: EditText = findViewById(R.id.editTextEmail)
-             email.text.toString().trim()
+            if (password.isEmpty() || password.length<6){
+                etPassword.error="Password harus lebih 6 karakter"
+                return@setOnClickListener
+            }
 
-             val  password: EditText = findViewById(R.id.editTextpassword)
-             password.text.toString().trim()
+            registerUser(email,password)
 
-             if (email.length()==0){
-                 email.error= "Email harus di isi!!!"
-                 email.requestFocus()
-                 return@setOnClickListener
-             }
+        }
 
+    }
 
-             if (password.length()==0 || password.length () < 8){
-                 password.error="Password harus lebih 8 karakter"
-                 password.requestFocus()
-                 return@setOnClickListener
-
-             }
-
-
-             registerUser(email = String(), password= String())
-         }
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
 
     }
 
@@ -53,7 +66,7 @@ class RegisterActivity : AppCompatActivity() {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this){
                 if (it.isSuccessful){
-                    val register = Intent(this@RegisterActivity,Home::class.java ).also {
+                    Intent(this@RegisterActivity, Home::class.java ).also {
                         it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                         startActivity(it)
 
@@ -66,9 +79,14 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     fun backLogin(view: View) {
-        val login = Intent(this@RegisterActivity,  MainActivity::class.java)
+        val login = Intent(this@RegisterActivity,  LoginActivity::class.java)
         startActivity(login)
 
     }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
+    }
+
 
 }

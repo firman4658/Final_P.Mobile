43002da1405f1193381b97656ef00c22838a1de3
firman4658 <78971161+firman4658@users.noreply.github.com>
2021-07-29 13:59:54 +0800

    hps kemarin

--- app/src/main/java/com/example/final_pmobile/KampusFragment.kt
+++ app/src/main/java/com/example/final_pmobile/KampusFragment.kt
@@ -5,17 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.final_pmobile.adapter.kampusadapter
-import com.example.final_pmobile.model.kampus
 
 
 class KampusFragment : Fragment() {
 
-    lateinit var kampusadapter: kampusadapter
-    val lm = LinearLayoutManager(activity)
-    val addKampusList: MutableList<kampus> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,14 +18,5 @@ class KampusFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_kampus, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-        initView()
-    }
-
-    fun initView() {
-        pm_kampus.layoutmanager
-
-    }
 }

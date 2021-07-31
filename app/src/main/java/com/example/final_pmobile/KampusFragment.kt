dae579fe1471f18bbfa272ec58511517f3b0fd2c
aswarthedoctor@gmail.com <68291451+Aswar12@users.noreply.github.com>
2021-07-31 23:16:58 +0800

    ASFA
    ASFAS

--- app/src/main/java/com/example/final_pmobile/KampusFragment.kt
+++ app/src/main/java/com/example/final_pmobile/KampusFragment.kt
@@ -1,14 +1,18 @@
 package com.example.final_pmobile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 
 
 class KampusFragment : Fragment() {
 
+    lateinit var  cv : CardView
+    private val Lokasi = MapsFragment()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +20,12 @@ class KampusFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_kampus, container, false)
+
+        cv.findViewById<CardView>(R.id.cv_akba).setOnClickListener {
+
+        }
+
+
     }
 
 
